01e6f159f74aa16b2c6b00fe6829e52b6c41cd2f
Laberon <laberon83@gmail.com>
2022-07-11 20:13:27 +0300

    end

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("gulliver = " + gulliver.getPublishingYear());
 
         System.out.println(new Book("Атлант расправил плечи",1957,new Author("Роман Айн","Рэнд")));
+        System.out.println(gulliver);
 
         Book gulliver2 = new Book("Gulliver's Travels", 2009, new Author("Jonatan", "Swift"));
         System.out.println(gulliver.equals(gulliver2));
